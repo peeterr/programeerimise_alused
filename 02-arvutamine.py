e124ce07cc20f0fa88c7bc86de7704ddac71fe19
Peeter Rätsepp <p.ratsepp@gmail.com>
2013-11-07 15:47:41 +0200

    Create 02-arvutamine.py

---
+++ 02-arvutamine.py
@@ -0,0 +1,47 @@
+
+# risttahukas
+print "Risttahukas"
+a = 10.0
+b = 15.0
+c = 20.0
+
+servad = 4.0 * (a + b + c)
+print "servad = {}".format(servad)
+
+pindala = 2.0 * (a * b + b * c + a * c)
+print "pindala = {}".format(pindala)
+
+ruumala = a * b * c
+print "ruumala = {}".format(ruumala)
+print
+# silinder
+print "Silinder"
+R = 4.0
+H = 6.0
+p = 3.14
+ruumala = R * R * H * p
+print "ruumala = {:.2}".format(ruumala)
+
+pindala = 2.0 * p * R * (R + H)
+print "pindala = {:.2}".format(pindala)
+print
+# kera
+print "Kera"
+R = 5.0
+p = 3.14
+ruumala = 4.0/3.0 * p * R * R * R
+print "ruumala = {:.2}".format(ruumala)
+
+pindala = 4.0/3.0 * p * R * R * R
+print "pindala = {}".format(pindala)
+print
+# pyramiid
+print "Pyramiid"
+A = 20
+H = 40.0
+kogupindala = A * A + (A / 2) * (H * H)
+print "kogupindala = {:.2}".format(kogupindala)
+
+ruumala = 1.0/3.0 * A * A * H
+print "ruumala = {:.2}".format(ruumala)
+
